ea81de07c27af882ba03372152dc77bd8ad425a1
n-stolz <nstolz@student.ethz.ch>
2021-12-06 15:09:53 +0100

    Cleaned Generate Pre-Built NC repo

--- Generate Pre-Built NC/main.py
+++ Generate Pre-Built NC/main.py
@@ -12,14 +12,9 @@ import pandas as pd
 import sys
 import datetime
 
-def get_random_year():
-    years = [2016]
-    selection = choice(years)
-    years.remove(selection)
-    #ONLY FOR DEBUGGING!
-    #selection=2016
-    return selection
 
+# copies all relevant data for a specific optimization year to build/model/national
+# AND divides timeseries of demand by mean demand in 2016 to account for long term trends
 def copy_year_to_model(demand_year):
     print('Demand and CF time series from: ', demand_year)
 
@@ -34,58 +29,69 @@ def copy_year_to_model(demand_year):
     demand_ts=pd.read_csv('build/model/national/electricity-demand.csv')
     for (columnName, columnData) in demand_ts.iteritems():
         if columnName != "utc_timestamp":
-            print(columnName)
+            #print(columnName)
             demand_ts[columnName] = demand_ts[columnName] * (
                         demand_ts_2016[columnName].mean() / demand_ts[columnName].mean())
             demand_ts.to_csv('build/model/national/electricity-demand.csv',index=False)
 
 
 def run_models():
+    #year is misleading - it is modelling step. Since we are only creating
     year=1
-    year_sequence = {'adjusted_costs:': {}, 'baseline': {}}
+
     #seed(123)
+    #todays date for specifying model run during saving of data
     today = datetime.datetime.now()
     date_time = today.strftime("%m-%d-%Y, %H:%M")
+
+    # define output Path depending on whether I run it on local machine or Euler
+    #TODO Bryn+Niklas: talk about shared data locations on cluster and adjust output_path
     if os.path.isdir('/home/niklas/European Incentive Model'):
         output_path = ('/home/niklas/European Incentive Model/output_{}'.format(date_time))
     else:
         output_path = ('/cluster/scratch/nstolz/output_{}'.format(date_time))
 
-    for model_year in [2016]:
+    #read argument - defines which weather/demand year is used
+    model_year=(sys.argv[5])
 
-        model = pipeline()
+    #create model instance - defined in model_pipeline.py
+    model = pipeline()
 
-        model.output_path=output_path
-        model.baseline_run=False
-
-
-        print('run_models thinks we are in year:',year)
-
-        model.ts_year=model_year
-        year_sequence['baseline']['step {}'.format(year)]=model.ts_year
-
-        copy_year_to_model(model.ts_year)
-        logging.info('running model run no: %s; demand and cf timeseries of year %s', year,model.ts_year)
-        print(year)
-        euro_calliope_specifications.fossil_share=config.energy_prod_model[['coal','ccgt']].sum(axis=1)-year*(1/specs.years)*config.energy_prod_model[['coal','ccgt']].sum(axis=1)
-        model.nuclear_scaling_factor = 1-3*year*(1/specs.years)
-
-
-        model.renewables_share = model.get_wind_pv_shares(config, year)
+    model.output_path=output_path
 
 
 
-        model.create_yaml_plan(euro_calliope_specifications.fossil_share, year, config.energy_prod_model)
+
+    print('run_models thinks we are in year:',year)
+
+    model.ts_year=model_year
+
+    #copy data to correct directory so model can execute it
+    copy_year_to_model(model.ts_year)
+
+    logging.info('running model run no: %s; demand and cf timeseries of year %s', year,model.ts_year)
+    print(year)
+
+    #
+    euro_calliope_specifications.fossil_share=config.energy_prod_model[['coal','ccgt']].sum(axis=1)-year*(1/specs.years)*config.energy_prod_model[['coal','ccgt']].sum(axis=1)
+    model.nuclear_scaling_factor = 1-3*year*(1/specs.years)
+
+
+    model.renewables_share = model.get_wind_pv_shares(config, year)
 
 
 
-        model.run_planning_model(year,euro_calliope_specifications.fossil_share,config.energy_prod_model)
+    model.create_yaml_plan(euro_calliope_specifications.fossil_share, year, config.energy_prod_model)
+
+
+
+    model.run_planning_model(year,euro_calliope_specifications.fossil_share,config.energy_prod_model)
         #energy_cap=get_energy_cap()
         #transmission_cap=get_transmission_cap()
         #storage_cap=get_storage_cap()
         #resource_cap=get_resource_cap()
 
-    
+
 
 
 
@@ -93,7 +99,8 @@ def run_models():
 if __name__ == '__main__':
     config=configuration()
     specs=model_specifications()
-    years=int(sys.argv[1])
+
+    
 
 
     euro_calliope_specs=euro_calliope_specifications()
